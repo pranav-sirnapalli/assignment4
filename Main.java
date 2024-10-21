a5dfa3305fbb29a953de27fe5e5f41f287d39d02
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2024-10-21 08:03:42 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package src;
+
+import controller.ImageController;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ImageController controller = new ImageController();
+
+        while (true) {
+            System.out.print("Enter command: ");
+            String command = scanner.nextLine();
+            controller.processCommand(command);
+        }
+    }
+}
